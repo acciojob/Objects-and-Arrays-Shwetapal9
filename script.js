072d3c923e9d84bc1b1af91671a5e60baa1aa4b6
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-16 22:00:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team = players;
-let team1 = {};
-for(int i=0;i< players.length;i++)
-{
-	team[i]=players[i];
-}
+let team1 = players;
+
 let cap1 = person
 window.players = players;
 window.person = person;
