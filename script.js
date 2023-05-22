92dd160a8e0cce910af3665ccdcb0a4019edd498
Shweta pal <51901918+Shwetapal9@users.noreply.github.com>
2023-05-22 14:40:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,5 +9,9 @@ const person = {
 let team = players;
 let team1 = [...players];
 
-let cap1 = {...person};
-
+let cap1 = {...person}
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
